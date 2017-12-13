96f63d62abbdea4a16d8e145c0625a9b8cc8cb00
David Knolls <Therobodavo@gmail.com>
2017-12-13 13:33:28 -0500

    Added square collision AABB

--- iteration1.js
+++ iteration1.js
@@ -32,12 +32,12 @@
 	{
 		ctx.clearRect(0,0,canvas.width,canvas.height);
 		ctx.fillStyle = 'gray';
-		ctx.fillRect(0,0,600,500);
+		ctx.fillRect(0,0,canvas.width,canvas.height);
 		
 		ctx.fillStyle = 'black';
 		ctx.beginPath();
 		ctx.moveTo(0,100);
-		ctx.lineTo(600,100);
+		ctx.lineTo(canvas.width,100);
 		ctx.stroke();
 
 		for(let i = 0; i < allShapes.length; i++)
@@ -52,9 +52,30 @@
 		{
 			canClick = true;
 		}
-		for(let i = 0; i < allShapes.length; i++)
+		for(let i = 0; i < allShapes.length;i++)
 		{
-			allShapes[i].y += 2;
+			let canMoveDown = true;
+			let tempYTop = allShapes[i].y + 2;
+			let tempYBottom = allShapes[i].y + 52;
+			for(let j = 0; j < allShapes.length; j++)
+			{
+				if(allShapes[i].x < allShapes[j].x + 50 &&
+				   allShapes[i].x + 50 > allShapes[j].x &&
+				   tempYTop < allShapes[j].y + 50 &&
+				   tempYBottom > allShapes[j].y)
+				{
+					canMoveDown = false;
+					break;
+				}
+			}
+			if(canMoveDown && allShapes[i].y + 52 <= canvas.height)
+			{
+				allShapes[i].y += 2;
+			}
+			else if(allShapes[i].y > canvas.height)
+			{
+				allShapes[i].y = canvas.height - 50;
+			}
 		}
 	}
 	function drawRect(x,y,color)
