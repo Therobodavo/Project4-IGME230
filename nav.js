e24b865234246540d801a6507bed2df244f9433b
David Knolls <Therobodavo@gmail.com>
2017-12-14 16:24:31 -0500

    Added javascript and css for nav

---
+++ nav.js
@@ -0,0 +1,12 @@
+document.querySelector("#iteration1").onclick = function(e)
+{
+    window.location = "iteration1.html";
+}
+document.querySelector("#iteration2").onclick = function(e)
+{
+    window.location = "iteration2.html";
+}
+document.querySelector("#iteration3").onclick = function(e)
+{
+    window.location = "iteration3.html";
+}
