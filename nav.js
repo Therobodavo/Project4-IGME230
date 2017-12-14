e3c5ef584efe2cd3af1fc083823bcdf724a8c267
David Knolls <Therobodavo@gmail.com>
2017-12-14 18:20:31 -0500

    fixed hyperlink capitalization

--- nav.js
+++ nav.js
@@ -1,14 +1,14 @@
 document.querySelector("#iteration1").onclick = function(e)
 {
-    window.location = "iteration1.html";
+    window.location = "Iteration1.html";
 };
 document.querySelector("#iteration2").onclick = function(e)
 {
-    window.location = "iteration2.html";
+    window.location = "Iteration2.html";
 };
 document.querySelector("#iteration3").onclick = function(e)
 {
-    window.location = "iteration3.html";
+    window.location = "Iteration3.html";
 };
 document.querySelector(".heading").onclick = function(e)
 {
